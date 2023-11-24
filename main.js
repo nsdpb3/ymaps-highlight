c404b1ee6164b2021ce8cdb3f26535d795555858
nsdbp3 <xramov14881@gmail.com>
2023-11-24 19:26:20 +0300

    Задокументировал код

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ class Map {
 	}
 	/**
 	 * Инициализация карты
+	 * @returns {Promise} Созданная карта
 	 */
 	initialize() {
 		return new Promise((resolve, reject) => {
@@ -28,10 +29,11 @@ class Map {
 	}
 	/**
 	 * Устанавливает метку и пушит её в общий массив меток
-	 * @param {object} coords - Координаты метки
+	 * @param {Array} coords - Координаты метки
 	 * @param {string} name - Название метки
 	 * @param {string} baloon - Балун метки
 	 * @param {string} preset - Оформление метки, по умолчанию => this.defaultPreset
+	 * @returns {Promise} Промис с ресолвом который содержит созданный марке
 	 */
 	setPlacemark(coords, name, baloon, preset = this.defaultPreset) {
 		return new Promise((resolve, reject) => {
@@ -50,12 +52,21 @@ class Map {
 			resolve(placemark);
 		});
 	}
+	/**
+	 * Пушит созданную метку в массив меток
+	 * @param {Array} placemark - Координаты метки
+	 * @returns {Promise} Промис с ресолвом который содержит массив меток
+	 */
 	pushMarkers(placemark) {
 		return new Promise((resolve) => {
 			this.myPlacemarks.push(placemark);
 			resolve(this.myPlacemarks);
 		});
 	}
+	/**
+	 * Сбрасывает оформление у всех меток
+	 * @returns {Promise} Промис с пустым resolve
+	 */
 	resetHighlight() {
 		return new Promise((resolve) => {
 			this.myMap.geoObjects.each((geoObject) => {
@@ -64,6 +75,11 @@ class Map {
 			resolve();
 		});
 	}
+	/**
+	 * Подсвечивает метку по переданным координатам
+	 * @param {Array} coords - Координаты метки
+	 * @returns {Promise} Промис с пустым ресолвом
+	 */
 	highlightPlacemark(coords) {
 		return new Promise((resolve, reject) => {
 			// Ищем метку с соответствующими координатами и подсвечиваем её
@@ -81,6 +97,11 @@ class Map {
 			}
 		});
 	}
+	/**
+	 * Приводит координаты к нужному виду
+	 * @param {Array} placemark - Координаты метки
+	 * @returns {Promise} Промис с ресолвом который содержит валидные координаты
+	 */
 	transformCoordinates(coordsNoTransform) {
 		return new Promise((resolve, reject) => {
 			coordsNoTransform = coordsNoTransform.split(",");
@@ -94,13 +115,13 @@ let center = [55.76, 37.64];
 let zoom = 11;
 
 let map = new Map(center, zoom, "map");
-let saloons = [
-	{
-		coords: [55.764374, 37.596857],
-		name: "Малый Козихинский переулок, 12",
-		ballon: "г. Москва, Малый Козихинский переулок, 12",
-	},
-];
+// let saloons = [
+// 	{
+// 		coords: [55.764374, 37.596857],
+// 		name: "Малый Козихинский переулок, 12",
+// 		ballon: "г. Москва, Малый Козихинский переулок, 12",
+// 	},
+// ];
 ymaps
 	.ready()
 	.then(() => map.initialize())
@@ -172,75 +193,6 @@ ymaps
 		);
 	})
 	.then((marker) => map.pushMarkers(marker));
-
-// ymaps.ready(() => {
-// 	map.initialize();
-// 	map.setPlacemark(
-// 		[55.764374, 37.596857],
-// 		"Малый Козихинский переулок, 12",
-// 		"г. Москва, Малый Козихинский переулок, 12"
-// 	);
-// 	map.setPlacemark(
-// 		[55.746674, 37.559092],
-// 		"Большая Дорогомиловская улица, 14к1",
-// 		"г. Москва, Большая Дорогомиловская улица, 14к1"
-// 	);
-// 	map.setPlacemark(
-// 		[55.764946, 37.58862],
-// 		"Садовая-Кудринская улица, 19с1",
-// 		"г. Москва, Садовая-Кудринская улица, 19с"
-// 	);
-// 	map.setPlacemark(
-// 		[55.748949, 37.655131],
-// 		"Земляной Вал, 52/16с1",
-// 		"г. Москва, улица Земляной Вал, 52/16с1"
-// 	);
-// 	map.setPlacemark(
-// 		[55.737451, 37.587596],
-// 		"Зубовский бульвар, 31-33",
-// 		"г. Москва, Зубовский бульвар, 31-33"
-// 	);
-// 	map.setPlacemark(
-// 		[55.737314, 37.6009],
-// 		"Коробейников переулок, 1",
-// 		"г. Москва, Коробейников переулок, 1"
-// 	);
-// 	map.setPlacemark(
-// 		[55.704793, 37.576286],
-// 		"Ленинский проспект, 43",
-// 		"г. Москва, Ленинский проспект, 43"
-// 	);
-// 	map.setPlacemark(
-// 		[55.736108, 37.248302],
-// 		"Жуковка, 58",
-// 		"Московская область, Одинцовский городской округ, деревня Жуковка, 58"
-// 	);
-// 	map.setPlacemark(
-// 		[59.959958, 30.303238],
-// 		"Большой проспект Петроградской стороны, 37",
-// 		"г. Санкт-Петербург, Большой проспект Петроградской стороны, 37"
-// 	);
-// 	map.setPlacemark(
-// 		[47.224936, 39.704673],
-// 		"Будённовский проспект, 49/97",
-// 		"г. Ростов-на-Дону, Будённовский проспект, 49/97"
-// 	);
-// 	map.setPlacemark(
-// 		[55.03976, 82.915498],
-// 		"улица Крылова, 4",
-// 		"г. Новосибирск, улица Крылова, 4"
-// 	);
-// 	map.setPlacemark(
-// 		[56.011877, 92.888307],
-// 		"Карла Маркса, 14",
-// 		"г. Красноярск, улица Карла Маркса, 14"
-// 	);
-// 	map.setPlacemark(
-// 		[47.224936, 39.704673],
-// 		"Будённовский проспект, 49/97",
-// 		"г. Ростов-на-Дону, Будённовский проспект, 49/97"
-// 	);
-// });
 document.addEventListener("DOMContentLoaded", function () {
 	let btns = document.querySelectorAll(".map-btn");
 	btns.forEach((btn) => {
